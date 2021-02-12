010bd2e4179eae83edfa6fdf0d5b853b094b0c09
Pavlovych Serhii <programmersp@gmail.com>
2021-02-12 19:03:18 +0200

    add js side-menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
  // Initialize and add the map
  function initMap() {
-     var map = L.map('map').setView([49.23525, 28.46973], 19);
+     var map = L.map('map').setView([49.2352, 28.4702], 16);
      var icon = L.icon({
          iconUrl: './assets/marker.svg',
          iconSize: [88, 91.03], // size of the icon
@@ -11,5 +11,14 @@
          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
      }).addTo(map);
 
-     L.marker([49.23525, 28.46973], { icon: icon }).addTo(map);
+     L.marker([49.2352, 28.4702], { icon: icon }).addTo(map);
+ }
+
+ function openMenu(){
+    //  let html=document.getElementsByTagName('html');
+    //  let overlay=document.getElementsByClassName('overlay');
+     let sidemenu=document.querySelector('.side-menu');
+    //  html.classList.add('overflow');
+    //  overlay.classList.add('overlay-show');
+     sidemenu.classList.add('side-menu-show');
  }
