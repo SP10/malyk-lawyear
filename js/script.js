11b9e3372dbcd2e01a8d4c39a45616042e9453ea
Serhii Pavlovych <programmersp@gmail.com>
2021-04-29 01:35:50 +0300

    add lang switcher

--- js/script.js
+++ js/script.js
@@ -66,21 +66,6 @@ const OnMenuItemClick = () => {
     });
 }
 
-const OnExpandList = () => {
-    let dropdown_link = document.querySelectorAll('.dropdown__link');
-    dropdown_link.forEach(link => {
-        link.addEventListener('click', (e) => {
-            let click = (link.dataset.click === 'true');
-            click = !click;
-            link.setAttribute('data-click', click);
-
-            let expand = (link.nextElementSibling.dataset.expand === 'true');
-            expand = !expand;
-            link.nextElementSibling.setAttribute('data-expand', expand);
-        }, false);
-    })
-}
-
 const OnOverlayClick = () => {
     let html = document.querySelector('html');
     let overlay = document.querySelector('.overlay');
@@ -89,7 +74,7 @@ const OnOverlayClick = () => {
         html.classList.remove('overflow');
         overlay.classList.remove('overlay-bg');
         sidemenu.classList.remove('side-menu__show');
-        CloseFeedBackForm();
+        // CloseFeedBackForm();
     }, false);
 }
 
@@ -126,13 +111,257 @@ const onInit = () => {
     }
 }
 
+/*auto menu functions*/
+
+const getLink = (path, title) => {
+    let link = document.createElement('a');
+    link.href = path;
+    link.text = title;
+    link.classList.add('nav__link');
+    return link;
+}
+
+const getSubList = (data) => {
+    let ul = document.createElement('ul');
+    ul.classList.add('dropdown__menu');
+    ul.setAttribute('data-expand', 'false');
+
+    if (data.items) {
+        for (item of data.items) {
+            let li = document.createElement('li');
+            li.classList.add('dropdown__item');
+            let link;
+            if (document.documentElement.lang === "ua-UA") {
+                link = getLink(item.link.UA, item.name.UA);
+            } else {
+                link = getLink(item.link.RU, item.name.RU);
+            }
+            li.appendChild(link);
+            ul.appendChild(li);
+        }
+    }
+    return ul;
+}
+
+const getList = (data) => {
+    let ul = document.createElement('ul');
+    ul.classList.add('dropdown__menu');
+    ul.setAttribute('data-expand', 'false');
+
+    for (item of data) {
+        if (item.items) {
+            let li = document.createElement('li');
+            li.classList.add('nav__item', 'dropdown');
+
+            let span = document.createElement('span');
+            span.classList.add('dropdown__link');
+            span.setAttribute('data-click', 'false');
+
+            if (document.documentElement.lang === "ua-UA") {
+                span.innerText = item.name.UA;
+            } else {
+                span.innerText = item.name.RU;
+            }
+
+            span.addEventListener('click', (handler) => {
+                let click = (handler.target.dataset.click === 'true');
+                click = !click;
+                handler.target.setAttribute('data-click', click);
+
+                let expand = (handler.target.nextElementSibling.dataset.expand === 'true');
+                expand = !expand;
+                handler.target.nextElementSibling.setAttribute('data-expand', expand);
+            }, false);
+
+            let subList = getSubList(item);
+
+            li.appendChild(span);
+            li.appendChild(subList);
+            ul.appendChild(li);
+        } else {
+            let li = document.createElement('li');
+            li.classList.add('dropdown__item');
+            let link = getLink(item.link, item.name);
+            if (document.documentElement.lang === "ua-UA") {
+                link = getLink(item.link.UA, item.name.UA);
+            } else {
+                link = getLink(item.link.RU, item.name.RU);
+            }
+            li.appendChild(link);
+            ul.appendChild(li);
+        }
+    }
+    return ul;
+}
+
+const createSideMenu = (menuObject, target) => {
+    for (const menuItem of menuObject) {
+        if (menuItem.submenus) {
+            let li = document.createElement('li');
+            li.classList.add('nav__item', 'dropdown');
+
+            let span = document.createElement('span');
+            span.classList.add('dropdown__link');
+            span.setAttribute('data-click', 'false');
+
+            if (document.documentElement.lang === "ua-UA") {
+                span.innerText = menuItem.name.UA;
+            } else {
+                span.innerText = menuItem.name.RU;
+            }
+
+            span.addEventListener('click', (handler) => {
+                let click = (handler.target.dataset.click === 'true');
+                click = !click;
+                handler.target.setAttribute('data-click', click);
+
+                let expand = (handler.target.nextElementSibling.dataset.expand === 'true');
+                expand = !expand;
+                handler.target.nextElementSibling.setAttribute('data-expand', expand);
+            }, false);
+
+            let submenu = getList(menuItem.submenus);
+            li.appendChild(span);
+            li.appendChild(submenu);
+            target.appendChild(li);
+        } else {
+            let li = document.createElement('li');
+
+            let link;
+            if (document.documentElement.lang === "ua-UA") {
+                link = getLink(menuItem.link.UA, menuItem.name.UA);
+            } else {
+                link = getLink(menuItem.link.RU, menuItem.name.RU);
+            }
+            li.classList.add('nav__item');
+            li.appendChild(link);
+            target.appendChild(li);
+        }
+    }
+}
+
+
+const onInitSideMenu = () => {
+    let menus = [
+        {
+            name: { 'UA': 'Головна', 'RU': 'Главная' },
+            link: { 'UA': "/", 'RU': '/ru' },
+        },
+        {
+            name: { 'UA': 'Біографія', 'RU': 'Биография' },
+            link: { 'UA': "/#about", 'RU': '/ru/#about' },
+        },
+        {
+            name: { 'UA': 'Послуги', 'RU': 'Услуги' },
+            submenus: [
+                {
+                    name: { 'UA': 'Сімейний адвокат', 'RU': 'Cемейный адвокат' },
+                    items: [
+                        {
+                            name: { 'UA': 'Розірвання шлюбу', 'RU': 'Расторжение брака' },
+                            link: { 'UA': "/service/family/rozirvannia-shliubu", 'RU': '' },
+                        },
+                        {
+                            name: { 'UA': 'Стягнення аліментів', 'RU': 'Взыскание алиментов' },
+                            link: { 'UA': "/service/family/stiahnennia-alimentiv", 'RU': '' },
+                        },
+                        {
+                            name: { 'UA': 'Розподіл спільного майна подружжя', 'RU': 'Разделе общего имущества супругов' },
+                            link: { 'UA': "/service/family/rozpodil-spilnoho-maina", 'RU': '' },
+                        },
+                        {
+                            name: { 'UA': 'Позбавлення батьківських прав', 'RU': 'Лишение родительских прав' },
+                            link: { 'UA': "/service/family/pozbavlennia-batkivskykh-prav", 'RU': '' },
+                        },
+                        {
+                            name: { 'UA': 'Встановлення батьківства', 'RU': 'Установление отцовства' },
+                            link: { 'UA': "/service/family/vstanovlennia-batkivstva", 'RU': '' },
+                        },
+                        {
+                            name: { 'UA': 'Усиновлення', 'RU': 'Усыновление' },
+                            link: { 'UA': "/service/family/usynovlennia", 'RU': '' },
+                        }
+                    ]
+                },
+                {
+                    name: { 'UA': 'Цивільне право', 'RU': 'Гражданское право' },
+                    link: { 'UA': "/#service", 'RU': '/#service' },
+                },
+                {
+                    name: { 'UA': 'Кримінальне право', 'RU': 'Уголовное право' },
+                    link: { 'UA': "/#service", 'RU': '/#service' },
+
+                },
+                {
+                    name: { 'UA': 'Адміністративне право', 'RU': 'Административное право' },
+                    link: { 'UA': "/#service", 'RU': '/#service' },
+
+                },
+                {
+                    name: { 'UA': 'Господарське право', 'RU': 'Хозяйственное право' },
+                    link: { 'UA': "/#service", 'RU': '/#service' },
+                },
+            ],
+        },
+        {
+            name: { 'UA': 'Вартість послуг', 'RU': 'Стоимость услуг' },
+            submenus: [
+                {
+                    name: { 'UA': 'Аналіз справи', 'RU': 'Анализ дела' },
+                    link: { 'UA': "/#analysis", 'RU': '/#analysis' },
+                },
+                {
+                    name: { 'UA': 'Адміністративні правопорушення', 'RU': 'Административные правонарушения' },
+                    link: { 'UA': "/#administrativeOff", 'RU': '/#administrativeOff' },
+                },
+                {
+                    name: { 'UA': 'Кримінальні справи', 'RU': 'Уголовные дела' },
+                    link: { 'UA': "/#criminal", 'RU': '/#criminal' },
+                },
+                {
+                    name: { 'UA': 'Цивільні справи', 'RU': 'Гражданские дела' },
+                    link: { 'UA': "/#civil", 'RU': '/#civil' },
+                },
+                {
+                    name: { 'UA': 'Сімейні справи', 'RU': 'Семейные дела' },
+                    link: { 'UA': "/#family", 'RU': '/#family' },
+                },
+                {
+                    name: { 'UA': 'Адміністративні справи', 'RU': 'Административные дела' },
+                    link: { 'UA': "/#administrative", 'RU': '/#administrative' },
+                },
+                {
+                    name: { 'UA': 'Господарські справи', 'RU': 'Хозяйственные дела' },
+                    link: { 'UA': "/#business", 'RU': '/#business' },
+                },
+            ],
+        },
+        {
+            name: { 'UA': 'Переваги', 'RU': 'Преимущества' },
+            link: { 'UA': "/#benefits", 'RU': '/#benefits' },
+        },
+        {
+            name: { 'UA': 'Новини', 'RU': 'Новости' },
+            link: { 'UA': "/#news", 'RU': '/#news' },
+        },
+        {
+            name: { 'UA': 'Контакти', 'RU': 'Контакты' },
+            link: { 'UA': "/#contacts", 'RU': '/#contacts' },
+        },
+    ];
+
+    let sideMenuId = document.getElementById('side-menu');
+    createSideMenu(menus, sideMenuId);
+}
+
 window.onload = () => {
     onInit();
     resize();
     onScroll();
     OnBurgerClick();
-    OnExpandList();
+    // OnExpandList();
     OnSideMenuClose();
     OnMenuItemClick();
     OnOverlayClick();
+    onInitSideMenu();
 }
